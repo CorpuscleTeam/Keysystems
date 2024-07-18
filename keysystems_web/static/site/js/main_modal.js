0655fbcba5097df3f79359137a9a999d40c94057
annagrudochko <fenisha74@gmail.com>
2024-07-18 16:03:55 +0400

    submit1

--- keysystems_web/static/site/js/main_modal.js
+++ keysystems_web/static/site/js/main_modal.js
@@ -1,4 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems);
+
+  // Обработчик нажатия на кнопку подтверждения в модальном окне
+  document.getElementById('confirmButton').addEventListener('click', function () {
+    document.getElementById('myForm').submit();
   });
+});
