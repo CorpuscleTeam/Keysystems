6d14054e599b92c2331d267cd0cc7543ba56c1df
dgush <dgushch@gmail.com>
2024-07-18 18:15:46 +0400

    auth done

--- keysystems_web/static/site/js/main_modal.js
+++ keysystems_web/static/site/js/main_modal.js
@@ -1,11 +1,9 @@
- console.log('dddddddddddddddddddddddddd')
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.modal');
   var instances = M.Modal.init(elems);
 
   // Обработчик нажатия на кнопку подтверждения в модальном окне
   document.getElementById('buttonConfirm').addEventListener('click', function () {
-    console.log('fffffffffffffffffffffff')
     document.getElementById('reg_form').submit();
   });
 });
