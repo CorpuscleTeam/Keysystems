061edc57d6d9eb4ea58412fb4e83c918c20cb0bb
annagrudochko <fenisha74@gmail.com>
2024-07-22 18:36:01 +0400

    index_4-8

--- keysystems_web/common/models.py
+++ keysystems_web/common/models.py
@@ -43,7 +43,7 @@ class Customer(models.Model):
 
 # пользователи
 class UserKS(AbstractUser):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='user', verbose_name='Клиент')
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='user', verbose_name='Клиент', null=True, blank=True)
     email = models.CharField('Почта', max_length=100, null=True, blank=True)
     full_name = models.CharField('ФИО', max_length=255, null=True, blank=True)
     phone = models.CharField('Телефон', max_length=100, null=True, blank=True)
