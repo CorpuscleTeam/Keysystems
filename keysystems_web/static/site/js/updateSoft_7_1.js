b0a01d57ad58925e9137abd96cdda42058c7fd97
annagrudochko <fenisha74@gmail.com>
2024-07-30 10:29:53 +0400

    297

--- keysystems_web/static/site/js/updateSoft_7_1.js
+++ keysystems_web/static/site/js/updateSoft_7_1.js
@@ -1,25 +1,24 @@
 /* {
-    "model": "client_app.news",
-    "pk": 3,
-    "fields": {
-        "created_at": "2024-07-29T13:34:16.120Z",
-        "updated_at": "2024-07-29T13:34:16.120Z",
-        "type_entry": "update",
-        "author": null,
-        "title": "ПК «Финконтроль-смарт»",
-        "text_preview": "Программный комплекс «Бюджет-СМАРТ» предназначен для автоматизации процессов составления, анализа и исполнения бюджета субъекта и бюджетов муниципальных образований. \r\nСоставление, уточнение бюджетной росписи, лимитов бюджетных обязательств;\r\nИсполнение бюджета по программно-целевому принципу;\r\nУчет бюджетных обязательств;\r\nКассовое обслуживание лицевых счетов бюджетных и автономных учреждений, бухгалтерский учет и отчетность по операциям со средствами бюджетных, автономных учреждений;\r\nВедение планов финансово-хозяйственной деятельности;",
-        "text": "Программный комплекс «Бюджет-СМАРТ» предназначен для автоматизации процессов составления, анализа и исполнения бюджета субъекта и бюджетов муниципальных образований. \r\nСоставление, уточнение бюджетной росписи, лимитов бюджетных обязательств;\r\nИсполнение бюджета по программно-целевому принципу;\r\nУчет бюджетных обязательств;\r\nКассовое обслуживание лицевых счетов бюджетных и автономных учреждений, бухгалтерский учет и отчетность по операциям со средствами бюджетных, автономных учреждений;\r\nВедение планов финансово-хозяйственной деятельности;",
-        "photo": "",
-        "is_active": true,
-        "date": "29 Июля 2024 / 13:34"
-    }
-} */
+     "date": "ВЧЕРА / 15:25",
+     "soft": "ПО 3",
+     "description": "Программный комплекс «WEB-Торги-КС» предоставляет возможности автоматизированной работы для следующих групп пользователей",
+     "update_files": [
+         {
+             "url": "/media/updates/5c23a3e243fb3167eb382be6.png",
+             "name": "updates/5c23a3e243fb3167eb382be6.png"
+         },
+         {
+             "url": "/media/updates/H8fJYUqYp9.jpeg",
+             "name": "updates/H8fJYUqYp9.jpeg"
+         }
+     ]
+ } */
 
 let updatePO = document.createElement('div')
 updatePO.classList.add('update_soft_flex')
 
-for (let i=0; i<updateSoft.length; i++) {
-    console.log(updateSoft[i])
+for (let i = 0; i < updateSoft.length; i++) {
+    // console.log(updateSoft[i])
     let updatePOitem = document.createElement('dev')
     updatePOitem.classList.add('updateItem')
 
@@ -29,12 +28,12 @@ for (let i=0; i<updateSoft.length; i++) {
 
     let header_left = document.createElement('h4')
     header_left.classList.add('update_title')
-    header_left.innerHTML = updateSoft[i]['fields']['title']
+    header_left.innerHTML = updateSoft[i]['soft']
     updateHeader.appendChild(header_left)
 
     let header_right = document.createElement('div')
     header_right.classList.add('update_date')
-    header_right.innerHTML = updateSoft[i]['fields']['date']
+    header_right.innerHTML = updateSoft[i]['date']
     updateHeader.appendChild(header_right)
 
     updatePO.appendChild(updatePOitem)
