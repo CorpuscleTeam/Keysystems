35fccca7bd40bfada2bb28c490c6e8ac9ddb2ebf
dgush <dgushch@gmail.com>
2024-08-21 16:05:46 +0400

    EDIT_STATUS

--- keysystems_web/common/consumers.py
+++ keysystems_web/common/consumers.py
@@ -87,18 +87,25 @@ class ChatConsumer(WebsocketConsumer):
             order_id = int(data_json.get('order_id', 0))
 
             # если есть кого добавить
-            # if data_json.get('add'):
-            #     add_user_id = int(data_json.get('add'))
-            #     OrderCurator.objects.create(order_id=order_id, user_id=add_user_id)
-            #
-            # # если есть кого удалить
-            # if data_json.get('del'):
-            #     del_user_id = int(data_json.get('del'))
-            #     OrderCurator.objects.create(order_id=order_id, user_id=del_user_id)
+            if data_json.get('add'):
+                add_user_id = int(data_json.get('add'))
+                OrderCurator.objects.create(order_id=order_id, user_id=add_user_id)
+
+            # если есть кого удалить
+            if data_json.get('del'):
+                del_user_id = int(data_json.get('del'))
+                OrderCurator.objects.create(order_id=order_id, user_id=del_user_id)
 
             async_to_sync(self.channel_layer.group_send)(
                 self.room_group_name, {"type": "curator.list", 'order_id': order_id}
             )
+
+        # изменить статус заказа
+        elif data_json['event'] == EditOrderAction.EDIT_STATUS:
+            order_id = int(data_json['order_id'])
+            order = Order.objects.filter(id=order_id)
+            order.status = data_json['status']
+            order.save()
 
     # обновляет сообщения в чате
     def chat_message(self, event):
