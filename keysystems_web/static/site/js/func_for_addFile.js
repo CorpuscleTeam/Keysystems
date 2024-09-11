5754da2e1c5756ddc82be3dd519b24424aee6171
annagrudochko <fenisha74@gmail.com>
2024-09-11 17:55:12 +0400

    fff

--- keysystems_web/static/site/js/func_for_addFile.js
+++ keysystems_web/static/site/js/func_for_addFile.js
@@ -9,7 +9,7 @@ function create_icon_path (ext) {
     return `../static/site/img/files/${file_name}.svg`
 }
 
-// функция для кнопки "скачать файл"
+// функция для div загруженного файла в заявке
 function dnlFile(selector, data) {
     let btnFile = document.createElement('div')
     btnFile.classList.add('update_file_btn')
@@ -36,6 +36,7 @@ function dnlFile(selector, data) {
     let FileName = document.createElement('p')
     FileName.classList.add('update_file_name')
     FileName.innerHTML = data['name']
+    console.log(data)
     FileCenter.appendChild(FileName)
 
     let FileSize = document.createElement('p')
@@ -45,7 +46,7 @@ function dnlFile(selector, data) {
 
     // картинка справа
     let FileRight = document.createElement('button')
-    FileRight.classList.add('update_file_dnl')
+    FileRight.classList.add('update_file_del')
     btnFileItem.appendChild(FileRight)
 
     let FileDnl = document.createElement('img')
@@ -58,5 +59,61 @@ function dnlFile(selector, data) {
     })
 }
 
-// 
+// кнопка скачать файл
 
+function addUpdateFile (parent, arr) {
+    let fileUrl = arr['url'] || arr['file_url'];
+    let fileName = arr['name'] || arr['filename'];
+    let filSize = arr['size'] || arr['file_size'];
+
+    let updateFile = document.createElement('button')
+    updateFile.classList.add('update_file_btn')
+    parent.appendChild(updateFile)
+
+    // ссылка на скачивание файла
+    let updateFileLink = document.createElement('a')
+   
+    updateFileLink.setAttribute('href', fileUrl)
+    updateFileLink.setAttribute('download', fileName);
+    updateFile.appendChild(updateFileLink)
+
+    let updateFileItem = document.createElement('div')
+    updateFileItem.classList.add('update_file_item')
+    updateFileLink.appendChild(updateFileItem)
+
+    // картинка слева
+    let updateFileLeft = document.createElement('div')
+    updateFileLeft.classList.add('update_file_img')
+    updateFileItem.appendChild(updateFileLeft)
+
+    let updateFileImg = document.createElement('img')
+    // добавить переменную
+    updateFileImg.setAttribute('src', arr['icon'])
+    updateFileLeft.appendChild(updateFileImg)
+
+    // центр - название файла, размер
+    let updateFileCenter = document.createElement('div')
+    updateFileCenter.classList.add('update_file_center')
+    updateFileItem.appendChild(updateFileCenter)
+
+    let updateFileName = document.createElement('p')
+    updateFileName.classList.add('update_file_name')
+    updateFileName.innerHTML = fileName
+    // updateFileName.innerHTML = arr['name']
+    updateFileCenter.appendChild(updateFileName)
+    
+    let updateFileSize = document.createElement('p')
+    updateFileSize.classList.add('update_file_size')
+    updateFileSize.innerHTML = filSize
+    // updateFileSize.innerHTML = arr['size']
+    updateFileCenter.appendChild(updateFileSize)
+
+    // картинка справа
+    let updateFileRight = document.createElement('div')
+    updateFileRight.classList.add('update_file_dnl')
+    updateFileItem.appendChild(updateFileRight)
+
+    let updateFileDnl = document.createElement('img')
+    updateFileDnl.setAttribute('src', fileDnl)
+    updateFileRight.appendChild(updateFileDnl)
+}
--- keysystems_web/static/site/js/updateSoft_7_2.js
+++ keysystems_web/static/site/js/updateSoft_7_2.js
@@ -18,60 +18,10 @@ let updateFiles_grid = document.createElement('div')
 updateFiles_grid.classList.add('update_files_grid')
 document.querySelector('.page_flex_body').appendChild(updateFiles_grid)
 
-function addUpdateFile (arr) {
-    let updateFile = document.createElement('button')
-    updateFile.classList.add('update_file_btn')
-    updateFiles_grid.appendChild(updateFile)
-
-    // ссылка на скачивание файла
-    let updateFileLink = document.createElement('a')
-    updateFileLink.setAttribute('href', arr['url'])
-    updateFileLink.setAttribute('download', arr['name']);
-    updateFile.appendChild(updateFileLink)
-
-    let updateFileItem = document.createElement('div')
-    updateFileItem.classList.add('update_file_item')
-    updateFileLink.appendChild(updateFileItem)
-
-    // картинка слева
-    let updateFileLeft = document.createElement('div')
-    updateFileLeft.classList.add('update_file_img')
-    updateFileItem.appendChild(updateFileLeft)
-
-    let updateFileImg = document.createElement('img')
-    // добавить переменную
-    updateFileImg.setAttribute('src', arr['icon'])
-    updateFileLeft.appendChild(updateFileImg)
-
-    // центр - название файла, размер
-    let updateFileCenter = document.createElement('div')
-    updateFileCenter.classList.add('update_file_center')
-    updateFileItem.appendChild(updateFileCenter)
-
-    let updateFileName = document.createElement('p')
-    updateFileName.classList.add('update_file_name')
-    updateFileName.innerHTML = arr['name']
-    updateFileCenter.appendChild(updateFileName)
-
-    let updateFileSize = document.createElement('p')
-    updateFileSize.classList.add('update_file_size')
-    updateFileSize.innerHTML = arr['size']
-    updateFileCenter.appendChild(updateFileSize)
-
-    // картинка справа
-    let updateFileRight = document.createElement('div')
-    updateFileRight.classList.add('update_file_dnl')
-    updateFileItem.appendChild(updateFileRight)
-
-    let updateFileDnl = document.createElement('img')
-    updateFileDnl.setAttribute('src', fileDnl)
-    updateFileRight.appendChild(updateFileDnl)
-}
-
 // загрузить файл
 for (let i = 0; i < updateSoft72['update_files'].length; i++) {
 
-    addUpdateFile (updateSoft72['update_files'][i])
+    addUpdateFile (updateFiles_grid, updateSoft72['update_files'][i])
     
     // let updateFile = document.createElement('button')
     // updateFile.classList.add('update_file_btn')
